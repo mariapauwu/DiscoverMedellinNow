565cf5550780b1196f05519b63879546d4f5f962
NicolasEstebanCuervo <rojascuervo942@gmail.com>
2023-10-05 18:02:27 -0500

    Commit arreglo link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RestauranteComponent } from "./Componentes/restaurante/restaurante.component";
 import { InicioComponent } from "./Componentes/Inicio/inicio.component";
-
-
+import { atraccionesComponent } from './Componentes/atracciones/atracciones.component';
 
 const routes: Routes = [
   {path: '', component: InicioComponent},
   {path: 'restaurante', component: RestauranteComponent},
+  {path: 'atracciones', component: atraccionesComponent},
   
 ];
 
